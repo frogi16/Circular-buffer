f4ebd6fc5cc8c5ae8b4109d81e02d1ad8127320d
Przemyslaw Skrobot <44038381+frogi16@users.noreply.github.com`>
2020-12-31 18:38:27 +0100

    Green

--- CircularBuffer.h
+++ CircularBuffer.h
@@ -4,7 +4,7 @@ template<size_t allocatedSize>
 class CircularBuffer
 {
 	size_t first = 0;
-	size_t size = 0;
+	size_t end = 0;
 	std::array<double, allocatedSize> vals;
 public:
 	constexpr size_t getAllocatedSize() const
@@ -14,17 +14,25 @@ public:
 
 	size_t getSize() const
 	{
-		return size;
+		return end >= first ? end - first : allocatedSize - first + end;
 	}
 
 	void add(double d)
 	{
-		vals[size++] = d;
+		vals[end] = d;
+		if (end == allocatedSize - 1)
+			end = 0;
+		else
+			end++;
 	}
 
 	double pop()
 	{
-		--size;
-		return vals[first++];
+		auto &toRet = vals[first];
+		if (first == allocatedSize - 1)
+			first = 0;
+		else
+			first++;
+		return toRet;
 	}
 };
