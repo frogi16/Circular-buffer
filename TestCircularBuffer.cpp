a01477295fb1ffc2f16f1f2c38a26860f92b9e23
Przemyslaw Skrobot <44038381+frogi16@users.noreply.github.com`>
2021-01-01 20:26:48 +0100

    Blue - introduce fixture

--- TestCircularBuffer.cpp
+++ TestCircularBuffer.cpp
@@ -6,65 +6,53 @@
 
 #include "CircularBuffer.h"
 
-TEST(TestBuffer, ConstructWithSize)
+using namespace ::testing;
+
+constexpr size_t bufferSize{ 5 };
+
+class CreateAndFillBufferCompletelyTest : public Test
 {
-	CircularBuffer<1> buffer;
-	ASSERT_EQ(buffer.getAllocatedSize(), 1);
-	ASSERT_EQ(buffer.getSize(), 0);
-}
+protected:
 
-TEST(TestBuffer, AddAndPopElement)
-{
-	CircularBuffer<3> buffer;
-	ASSERT_EQ(buffer.getSize(), 0);
-	ASSERT_EQ(buffer.getAllocatedSize(), 3);
-
-	buffer.add(3.14);
-	ASSERT_EQ(buffer.getSize(), 1);
-	ASSERT_EQ(buffer.getAllocatedSize(), 3);
-
-	ASSERT_DOUBLE_EQ(buffer.pop(), 3.14);
-	ASSERT_EQ(buffer.getSize(), 0);
-	ASSERT_EQ(buffer.getAllocatedSize(), 3);
-}
-
-TEST(TestBuffer, RetrieveElementsInOrderOfAdding)
-{
-	CircularBuffer<5> buffer;
-	std::array<double, 5> vals;
-	std::iota(vals.begin(), vals.end(), -3);
-
-	for (size_t i = 0; i < vals.size(); i++)
+	void SetUp() override
 	{
-		buffer.add(vals[i]);
-		ASSERT_EQ(buffer.getSize(), i+1);
+		EXPECT_EQ(buffer.getAllocatedSize(), bufferSize);
+		EXPECT_EQ(buffer.getSize(), 0);
+
+		std::iota(vals.begin(), vals.end(), -3);
+
+		for (size_t i = 0; i < vals.size(); i++)
+		{
+			buffer.add(vals[i]);
+			EXPECT_EQ(buffer.getSize(), i + 1);
+		}
+
+		EXPECT_EQ(buffer.getAllocatedSize(), bufferSize);
+		EXPECT_EQ(buffer.getSize(), bufferSize);
 	}
 
+	void TearDown() override
+	{
+		EXPECT_EQ(buffer.getAllocatedSize(), bufferSize);
+	}
+
+	CircularBuffer<bufferSize> buffer;
+	std::array<double, bufferSize> vals;
+};
+
+TEST_F(CreateAndFillBufferCompletelyTest, RetrieveElementsInOrderOfAdding)
+{
 	for (auto &v : vals)
 		ASSERT_EQ(buffer.pop(), v);
 }
 
-TEST(TestBuffer, AddTooManyElements_shouldNotFail)
+TEST_F(CreateAndFillBufferCompletelyTest, AddTooManyElements_shouldNotFail)
 {
-	CircularBuffer<5> buffer;
-	std::array<double, 5> vals;
-	std::iota(vals.begin(), vals.end(), -3);
-
-	for (auto &v : vals)
-		buffer.add(v);
-
 	ASSERT_NO_THROW(buffer.add(0));
 }
 
-TEST(TestBuffer, AddTooManyElements_shouldOverwriteOldestAndReturnLast)
+TEST_F(CreateAndFillBufferCompletelyTest, AddTooManyElements_shouldOverwriteOldestAndReturnLast)
 {
-	CircularBuffer<5> buffer;
-	std::array<double, 5> vals;
-	std::iota(vals.begin(), vals.end(), -3);
-
-	for (auto &v : vals)
-		buffer.add(v);
-
 	buffer.add(0);
 
 	for (size_t i = 1; i < vals.size(); i++)
